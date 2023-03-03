ab6df07293b3d0b7d400a7e9e5c4a8342f12cf8c
CaioKenedy <caiokenedysantos@gmail.com>
2023-03-03 15:27:52 -0300

    setting tailwind

---
+++ web/src/App.tsx
@@ -0,0 +1,17 @@
+import './styles/global.css';
+
+import { Habit } from "./components/Habit"
+
+function App() {
+  return (
+   <div>
+    <Habit completed={10} />
+    <Habit completed={3} />
+    <Habit completed={5} />
+    <Habit completed={1} />
+   </div>
+    
+  )
+}
+
+export default App
